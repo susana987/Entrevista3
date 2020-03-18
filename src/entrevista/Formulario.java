6edbff066bdc1ba6b45bd17952c3cf15dcb1d901
susanaalba <susanaalba159@gmail.com>
2020-03-18 16:38:33 -0600

    subiendo proyecto

---
+++ src/entrevista/Formulario.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package entrevista;
+
+/**
+ *
+ * @author Susana Alba
+ */
+public class Formulario {
+    
+    public String Mate (int a, int b){
+       String msg = null;
+       if (a > b){
+          msg = "EL NUMERO 1 ES MAYOR"; 
+       }else{
+           msg = "EL NUMERO 2 ES MAYOR";
+       }
+    return msg;   
+    }
+    
+  // prueba  
+    
+    
+}
